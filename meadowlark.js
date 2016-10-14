25598f8afd6ef8e7d17a88da006e4ad93869f241
Admin <ZhangMing@pasc.com.cn>
2016-10-14 22:05:59 +0800

    learn to single page test

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var fortune=require('./lib/fortune.js');
+var fortune = require('./lib/fortune.js');
 
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
@@ -11,11 +11,20 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
 // 404 catch-all handler (middleware)
